a844ea0ea2e4d0e7f08d2967f469f4f11ced8eb4
Yanislav <yanislav.angelov04@gmail.com>
2025-03-20 18:33:14 +0200

    firebase setup

---
+++ bullionhaven/src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCiv58hCV37K-ncWQv5oPvxDe_P0KzPhzA",
+  authDomain: "bullion-haven-5726c.firebaseapp.com",
+  projectId: "bullion-haven-5726c",
+  storageBucket: "bullion-haven-5726c.firebasestorage.app",
+  messagingSenderId: "476455709849",
+  appId: "1:476455709849:web:858c8921544439a47edc08",
+  measurementId: "G-LNFQTEEYBK"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
